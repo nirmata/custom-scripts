39bd12085c0dd267df482baa7ddf0d3530a99953
bitbucket90 <brandon@forerivertech.com>
2018-11-05 19:01:53 -0800

    Added correct namespace
    -n nirmata-shared

--- nirmatarestart.sh
+++ nirmatarestart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # nadm nirmata agent restart
 # 11/2/2018 Nirmata
-kubectl delete pod -n nirmata $(kubectl get pods -n nirmata|grep nirmata-agent |awk '{print $1}')
+kubectl delete pod -n nirmata $(kubectl get pods -n nirmata-shared|grep nirmata-agent |awk '{print $1}')
