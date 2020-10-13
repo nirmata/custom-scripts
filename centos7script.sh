ca9f16fbe0a90e9684e0198dd07605030707bb0e
Samuel Silbory <sam@nirmata.com>
2020-10-13 13:58:01 -0700

    clean scripts

--- centos7script.sh
+++ centos7script.sh
@@ -1,19 +1,46 @@
 #!/bin/bash -x 
-# Install Docker 
-sudo yum update -y 
+sudo -i
 
-sudo tee /etc/yum.repos.d/docker.repo <<-'EOF'
-[dockerrepo]
-name=Docker Repository
-baseurl=https://yum.dockerproject.org/repo/main/centos/7/
-enabled=1
-gpgcheck=1
-gpgkey=https://yum.dockerproject.org/gpg 
-EOF
+# Setup Firewall
+#systemctl disable firewalld
+firewall-cmd --permanent --add-port=2379-2380/tcp
+firewall-cmd --permanent --add-port=8472/udp
+firewall-cmd --permanent --add-port=30000-32767/tcp
+firewall-cmd --add-masquerade --permanent
 
-sudo mount -o remount,rw '/sys/fs/cgroup' 
-sudo ln -s /sys/fs/cgroup/cpu,cpuacct /sys/fs/cgroup/cpuacct,cpu 
+# Node ports
+firewall-cmd --permanent --add-port=10250-10255/tcp
 
-sudo yum install -y docker-engine
-sudo systemctl enable docker.service
-sudo systemctl start docker
+# Really only the master nodes
+firewall-cmd --permanent --add-port=6443/tcp
+
+# Setup repos
+yum-config-manager --add-repo https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+yum-config-manager --enable rhel-7-server-extras-rpms
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum-config-manager --add-repo https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+
+yum install -y yum-utils device-mapper-persistent-data lvm2 yum-plugin-versionlock containerd.io docker-ce kubectl 
+yum update -y
+# Automatic upgrades of docker = cluster death
+yum versionlock docker-ce-*
+
+# kubeadm cluster
+#yum install -y yum-utils device-mapper-persistent-data   lvm2 yum-plugin-versionlock containerd.io docker-ce kubelet-1.16.15 kubeadm-1.16.15 kubectl-1.16.15 
+# Automatic upgrades of docker and K8 = cluster death
+#yum versionlock kubelet-* kubeadm-* kubectl-* docker-ce-*
+
+# Basic Linux Kernel settings
+echo br_netfilter > /etc/modules-load.d/br_netfilter
+modprobe br_netfilter
+sysctl -w net.ipv4.ip_forward=1
+echo ‘net.ipv4.ip_forward=1’ >> /etc/sysctl.conf
+
+# Turning off selinux is generally required to make things work.
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+#In case you altered docker in some way.
+systemctl reload docker
+systemctl start docker
+systemctl enable docker
--- cleanup-cluster.sh
+++ cleanup-cluster.sh
@@ -1,31 +1,43 @@
 #!/bin/bash
 # Nirmata cleanup script
 
-# Stop and remove any running containers
-sudo docker stop $(sudo docker ps | grep “flannel” | gawk '{print $1}')
-sudo docker stop $(sudo docker ps | grep "nirmata" | gawk '{print $1}')
-sudo docker stop $(sudo docker ps | grep "kube" | gawk '{print $1}')
-sudo docker rm  $(sudo docker ps -a | grep "Exit" |gawk '{print $1}')
+sudo -i 
 
-# Remove any cni plugins
-sudo rm -rf /etc/cni/*
-sudo rm -rf /opt/cni/*
+# Clean Nirmata agent
+sudo systemctl stop nirmata-agent.service
+sudo systemctl disable nirmata-agent.service
+sudo rm -rf /etc/systemd/system/nirmata-agent.service
+
+# Cleanup docker 
+docker rm -f $(docker ps -qa)
+docker volume rm $(docker volume ls -q)
+
+# Clean anything that could possible leave stuff behind.
+cleanupdirs="/var/lib/etcd /etc/kubernetes /etc/cni /opt/cni /var/lib/cni /var/run/calico /opt/rke /etc/cni/ /opt/cni/ /data /opt/nirmata"
+for dir in $cleanupdirs; do
+  echo "Removing $dir"
+  rm -rf $dir
+done
+
+# Disable kubelet 
+systemctl disable kubelet
+kubeadm reset -f
+
 
 # Clear IP Tables
 sudo iptables --flush
 sudo iptables -tnat --flush
 
 # Restart Docker
-sudo systemctl stop docker
-sudo systemctl start docker
-sudo docker ps
+# sudo systemctl restart docker
 
 # Deletes the CNI interface
-sudo ifconfig cni0 down
-sudo brctl delbr cni0
-sudo ifconfig flannel.1 down
-sudo ip link delete cni0
-sudo ip link delete flannel.1
+#sudo ifconfig cni0 down
+#sudo brctl delbr cni0
+#sudo ifconfig flannel.1 down
+#sudo ip link delete cni0
+#sudo ip link delete flannel.1
 
-# Remove cluster database
-sudo rm -rf /data
+echo Rebooting System to flush kernel, and any hung containers.
+reboot
+
