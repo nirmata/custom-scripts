88c97146f6f9b21876a905a63a6b2e552b60f1b8
Anubhav Sharma <40705688+anubhav888@users.noreply.github.com>
2021-02-08 16:33:16 -0800

    Update cleanup-cluster.sh

--- cleanup-cluster.sh
+++ cleanup-cluster.sh
@@ -4,6 +4,7 @@
 # Stop and remove any running containers
 sudo docker stop $(sudo docker ps | grep “flannel” | gawk '{print $1}')
 sudo docker stop $(sudo docker ps | grep "nirmata" | gawk '{print $1}')
+
 sudo docker stop $(sudo docker ps | grep "kube" | gawk '{print $1}')
 sudo docker rm  $(sudo docker ps -a | grep "Exit" |gawk '{print $1}')
 
@@ -23,9 +24,15 @@ sudo docker ps
 # Deletes the CNI interface
 sudo ifconfig cni0 down
 sudo brctl delbr cni0
+sudo ifconfig kube-bridge down
 sudo ifconfig flannel.1 down
+sudo ifconfig | grep tun- | awk -F ':' '{print $1}' | xargs -t -I % sh -c '{ ifconfig % down;}'
+sudo ifconfig | grep tun- | awk -F ':' '{print $1}' | xargs -t -I % sh -c '{ ip link delete %;}'
+sudo ip link | grep tun | awk '{print $2}' | awk -F '@' '{print $1}' | xargs -t -I % sh -c '{ ip link delete %;}'
+sudo ip link delete kube-bridge
 sudo ip link delete cni0
 sudo ip link delete flannel.1
 
 # Remove cluster database
-sudo rm -rf /data
+sudo rm -rf /data/fixtures
+sudo rm -rf /data/member
