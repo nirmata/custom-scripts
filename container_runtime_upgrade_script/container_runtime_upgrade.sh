e89b9802db37edf551b154c638cca347d48fbe43
Vikash Kaushik <111422528+vikashkaushik01@users.noreply.github.com>
2023-02-13 23:16:12 +0530

    Updated the script

--- container_runtime_upgrade_script/container_runtime_upgrade.sh
+++ container_runtime_upgrade_script/container_runtime_upgrade.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# check if containerd is installed
 
+# check if containerd is installed
 echo "==============================================="
 echo "Checking if containerd is installed"
 echo "==============================================="
@@ -14,27 +14,50 @@ if ! command -v containerd > /dev/null; then
     sudo yum install -y containerd
   fi
 fi
+
+# configure Docker repository
+echo "==============================================="
+echo "Configuring Docker repository"
+echo "==============================================="
+if [ -f /etc/debian_version ]; then
+  sudo apt-get update
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+elif [ -f /etc/redhat-release ]; then
+  sudo yum install -y yum-utils
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+fi
+
+# upgrade Docker to latest version
+echo "==============================================="
+echo "Upgrading Docker to the latest version"
+echo "==============================================="
+if [ -f /etc/debian_version ]; then
+  sudo apt-get update
+  sudo apt-get upgrade -y docker-ce docker-ce-cli containerd.io
+elif [ -f /etc/redhat-release ]; then
+  sudo yum upgrade -y docker-ce docker-ce-cli containerd.io
+fi
+
 # check for the latest version of Docker
 echo "==============================================="
-echo "Checking if docker is up-to-date"
+echo "Checking if Docker is up-to-date"
 echo "==============================================="
 current_version=$(docker version --format '{{.Client.Version}}')
 latest_version=$(curl --silent https://download.docker.com/linux/static/stable/x86_64/ | grep -o "docker-[0-9.]*\.tgz" | sort -V | tail -n 1 | cut -d '-' -f 2 | cut -d '.' -f 1,2)
 if [ "$current_version" != "$latest_version" ]; then
-  echo "Docker is not up-to-date, upgrading now..."
-  # upgrade Docker based on the OS
-  if [ -f /etc/debian_version ]; then
-    sudo apt-get update
-    sudo apt-get upgrade -y docker
-  elif [ -f /etc/redhat-release ]; then
-    sudo yum upgrade -y docker
-  fi
+  echo "Docker upgrade failed, current version: $current_version, latest version: $latest_version"
+else
+  echo "Docker upgrade succeeded, current version: $current_version, latest version: $latest_version"
 fi
+
 # remove config.toml file
 echo "==============================================="
 echo "Removing containerd config.toml file"
 echo "==============================================="
 sudo rm -f /etc/containerd/config.toml
+
 # restart containerd
 echo "==============================================="
 echo "Resarting Containerd"
