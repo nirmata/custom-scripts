9cdeb71ac8aab4fea317639cde5197af8ccc9740
nsagark <90008930+nsagark@users.noreply.github.com>
2023-04-20 11:15:18 -0400

    Update list-users.sh

--- list-users-api/list-users.sh
+++ list-users-api/list-users.sh
@@ -11,6 +11,15 @@ fi
 echo -e "\nEnter the Nirmata API token: \n"
 read -s TOKEN
 
+echo "-------------------------------------------------"
+echo "Generating the csv file with the list of users..."
+echo "-------------------------------------------------"
 
 echo "Name,ID,IdentityProvider" > users.csv
-curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/users/api/User" | jq -r '.[] | "\(.name),\(.id),\(.identityProvider)"' >> users.csv
+curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/users/api/User" | jq -r '.[] | "\(.name),\(.id),\(.identityProvider)"' 2> /dev/null >> users.csv
+
+if [[ $? = 0 ]]; then
+        echo -e "\nusers.csv file generated successfully"
+else
+        echo -e "\nSomething went wrong when generating the csv file. Please check your script for any errors"
+fi
