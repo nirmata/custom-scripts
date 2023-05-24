f000361201947b1298205ab704ed830d8d53c8a7
Anudeep Nalla <121174814+anuddeeph1@users.noreply.github.com>
2023-05-25 00:44:23 +0530

    Update update_image_repo.sh

--- upload-nirmata-images-to-private-repo/update_image_repo.sh
+++ upload-nirmata-images-to-private-repo/update_image_repo.sh
@@ -57,3 +57,57 @@ docker tag ghcr.io/nirmata/kubernetes-zookeeper:v3-zk3.6.3 ${PRIVATE_REPO_NAME}/
 echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/kubernetes-zookeeper:v3-zk3.6.3 to private repository ..."
 docker push ${PRIVATE_REPO_NAME}/nirmata/kubernetes-zookeeper:v3-zk3.6.3
 echo
+
+echo
+echo "Pulling image ghcr.io/nirmata/enterprise-kyverno-operator:v0.1.7 from Nirmata repository ..."
+docker pull ghcr.io/nirmata/enterprise-kyverno-operator:v0.1.7
+echo "Tagging downloaded image ghcr.io/nirmata/enterprise-kyverno-operator:v0.1.7 to ${PRIVATE_REPO_NAME}/nirmata/enterprise-kyverno-operator:v0.1.7"
+docker tag ghcr.io/nirmata/enterprise-kyverno-operator:v0.1.7 ${PRIVATE_REPO_NAME}/nirmata/enterprise-kyverno-operator:v0.1.7
+echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/enterprise-kyverno-operator:v0.1.7 to private repository ..."
+docker push ${PRIVATE_REPO_NAME}/nirmata/enterprise-kyverno-operator:v0.1.7
+echo
+
+echo
+echo "Pulling image ghcr.io/nirmata/kyverno-license-manager:v0.0.2 from Nirmata repository ..."
+docker pull ghcr.io/nirmata/kyverno-license-manager:v0.0.2
+echo "Tagging downloaded image ghcr.io/nirmata/kyverno-license-manager:v0.0.2 to ${PRIVATE_REPO_NAME}/nirmata/kyverno-license-manager:v0.0.2"
+docker tag ghcr.io/nirmata/kyverno-license-manager:v0.0.2 ${PRIVATE_REPO_NAME}/nirmata/kyverno-license-manager:v0.0.2
+echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/kyverno-license-manager:v0.0.2 to private repository ..."
+docker push ${PRIVATE_REPO_NAME}/nirmata/kyverno-license-manager:v0.0.2
+echo
+
+echo
+echo "Pulling image ghcr.io/nirmata/kyverno:v1.9.2-n4k.nirmata.1 from Nirmata repository ..."
+docker pull ghcr.io/nirmata/kyverno:v1.9.2-n4k.nirmata.1
+echo "Tagging downloaded image ghcr.io/nirmata/kyverno:v1.9.2-n4k.nirmata.1 to ${PRIVATE_REPO_NAME}/nirmata/kyverno:v1.9.2-n4k.nirmata.1"
+docker tag ghcr.io/nirmata/kyverno:v1.9.2-n4k.nirmata.1 ${PRIVATE_REPO_NAME}/nirmata/kyverno:v1.9.2-n4k.nirmata.1
+echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/kyverno:v1.9.2-n4k.nirmata.1 to private repository ..."
+docker push ${PRIVATE_REPO_NAME}/nirmata/kyverno:v1.9.2-n4k.nirmata.1
+echo
+
+echo
+echo "Pulling image ghcr.io/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1 from Nirmata repository ..."
+docker pull ghcr.io/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1
+echo "Tagging downloaded image ghcr.io/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1 to ${PRIVATE_REPO_NAME}/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1"
+docker tag ghcr.io/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1 ${PRIVATE_REPO_NAME}/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1
+echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1 to private repository ..."
+docker push ${PRIVATE_REPO_NAME}/nirmata/kyvernopre:v1.9.2-n4k.nirmata.1
+echo
+
+echo
+echo "Pulling image ghcr.io/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1 from Nirmata repository ..."
+docker pull ghcr.io/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1
+echo "Tagging downloaded image ghcr.io/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1 to ${PRIVATE_REPO_NAME}/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1"
+docker tag ghcr.io/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1 ${PRIVATE_REPO_NAME}/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1
+echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1 to private repository ..."
+docker push ${PRIVATE_REPO_NAME}/nirmata/cleanup-controller:v1.9.2-n4k.nirmata.1
+echo
+
+echo
+echo "Pulling image ghcr.io/nirmata/metrics-agent:0.38.2 from Nirmata repository ..."
+docker pull ghcr.io/nirmata/metrics-agent:0.38.2
+echo "Tagging downloaded image ghcr.io/nirmata/metrics-agent:0.38.2 to ${PRIVATE_REPO_NAME}/nirmata/metrics-agent:0.38.2"
+docker tag ghcr.io/nirmata/metrics-agent:0.38.2 ${PRIVATE_REPO_NAME}/nirmata/metrics-agent:0.38.2
+echo "Uploading image ${PRIVATE_REPO_NAME}/nirmata/metrics-agent:0.38.2 to private repository ..."
+docker push ${PRIVATE_REPO_NAME}/nirmata/metrics-agent:0.38.2
+echo
