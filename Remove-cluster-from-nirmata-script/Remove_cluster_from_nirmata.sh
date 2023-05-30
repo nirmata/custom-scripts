eca017da3599f7c7c69d3fc82b5958b2a1ee9828
Anudeep Nalla <121174814+anuddeeph1@users.noreply.github.com>
2023-05-30 15:17:50 +0530

    Update Remove_cluster_from_nirmata.sh

--- Remove-cluster-from-nirmata-script/Remove_cluster_from_nirmata.sh
+++ Remove-cluster-from-nirmata-script/Remove_cluster_from_nirmata.sh
@@ -7,7 +7,7 @@ if [ $# -lt 1 ]; then
     echo ""
     echo "Eg: $0 test-cluster"
 else
-	TOKEN=DsmghHJ13w8sxjAtkK2Pp9taNQROJRjCzzc7L/osaewvQpFuJ7tjP2euNlvSKXaIXZc9lA7w6fYQZdxET8clZA==
+	TOKEN=<Add Nirmata API Token Here>
 	NIRMATAURL=https://www.nirmata.io
 	CLUSTERNAME=$1
 	CLUSTERID=$(curl -s -H "Accept: application/json, text/javascript, */*; q=0.01" -H "Authorization: NIRMATA-API $TOKEN" -X GET "$NIRMATAURL/cluster/api/KubernetesCluster?fields=id,name" | jq -r ".[] | select( .name == \"$CLUSTERNAME\" ).id")
