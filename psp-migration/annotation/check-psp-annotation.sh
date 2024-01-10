8b8f5ae3ee3b42bae7a5b204e9be79fcd09c7093
nsagark <90008930+nsagark@users.noreply.github.com>
2024-01-10 12:23:02 -0500

    Update check-psp-annotation.sh

--- psp-migration/annotation/check-psp-annotation.sh
+++ psp-migration/annotation/check-psp-annotation.sh
@@ -3,6 +3,7 @@
 # Initialize counters
 annotated_pods=0
 non_annotated_pods=0
+empty_namespaces=()
 
 # Print header with fixed column widths
 echo
@@ -12,21 +13,29 @@ echo "--------------------------------------------------------------------------
 
 # Loop through all namespaces
 for ns in $(kubectl get namespaces -o custom-columns=NAME:.metadata.name --no-headers); do
-  # Loop through pods in each namespace
-  for pod in $(kubectl get pods -n $ns -o custom-columns=NAME:.metadata.name --no-headers); do
-    # Check if the pod has the "kubernetes.io/psp" annotation
-    annotation_value=$(kubectl get pod $pod -n $ns -o jsonpath='{.metadata.annotations.kubernetes\.io/psp}')
+  # Get pods in each namespace
+  pods=$(kubectl get pods -n "$ns" -o custom-columns=NAME:.metadata.name --no-headers)
 
-    if [ -n "$annotation_value" ]; then
-      # Pod has the annotation
-      printf "%-55s %-20s %-20s %s\n" "$pod" "$ns" "Yes" "$annotation_value"
-      ((annotated_pods++))
-    else
-      # Pod does not have the annotation
-      printf "%-55s %-20s %-20s %s\n" "$pod" "$ns" "No" "N/A"
-      ((non_annotated_pods++))
-    fi
-  done
+  if [ -z "$pods" ]; then
+    # No pods in this namespace
+    empty_namespaces+=("$ns")
+  else
+    # Loop through pods
+    for pod in $pods; do
+      # Check if the pod has the "kubernetes.io/psp" annotation
+      annotation_value=$(kubectl get pod "$pod" -n "$ns" -o jsonpath='{.metadata.annotations.kubernetes\.io/psp}')
+
+      if [ -n "$annotation_value" ]; then
+        # Pod has the annotation
+        printf "%-55s %-20s %-20s %s\n" "$pod" "$ns" "Yes" "$annotation_value"
+        ((annotated_pods++))
+      else
+        # Pod does not have the annotation
+        printf "%-55s %-20s %-20s %s\n" "$pod" "$ns" "No" "N/A"
+        ((non_annotated_pods++))
+      fi
+    done
+  fi
 done
 
 echo "----------------------------------------------------------------------------------------------------------------------"
@@ -34,4 +43,15 @@ echo "--------------------------------------------------------------------------
 echo
 echo "Total Pods with psp annotation: $annotated_pods"
 echo "Total Pods without psp annotation: $non_annotated_pods"
-echo
+
+# Print namespaces with 0 pods
+if [ ${#empty_namespaces[@]} -gt 0 ]; then
+    echo
+    echo "Namespaces with 0 Pods:"
+    for ns in "${empty_namespaces[@]}"; do
+        echo "- $ns"
+    done
+else
+    echo
+    echo "No namespaces with 0 pods."
+fi
