6af7371e86a63945f55e7d972ee0dc078e666f43
rootxrishabh <rishabh.soni@nirmata.com>
2024-08-29 19:43:15 +0530

    Merge novartis_test.sh with nirmata_test.sh
    Signed-off-by: rootxrishabh <rishabh.soni@nirmata.com>

--- nirmata_test/nirmata_test.sh
+++ nirmata_test/nirmata_test.sh
@@ -735,7 +735,60 @@ spec:
       kubectl --namespace=$ns delete ds nirmata-net-test-all --ignore-not-found=true &>/dev/null
     done
 
+    # Test if metrics service is enabled
+    if kubectl get deployment -n kube-system metrics-server > /dev/null 2>&1; then
+        good "Metric server is installed."
+    else
+        warn "Metric server is not installed."
+    fi
 
+    NAMESPACE="devtest5"
+
+    # Get all deployments in the specified namespace
+    deployments=$(kubectl get deployments -n "$NAMESPACE" -o json)
+
+    # Check if there are any deployments
+    if [[ $(echo "$deployments" | jq '.items | length') -eq 0 ]]; then
+        echo "No deployments found in namespace $NAMESPACE."
+        exit 0
+    fi
+
+    # Initialize a flag to track the status
+    all_deployments_ok=true
+
+    # Loop through each deployment
+    echo "$deployments" | jq -c '.items[]' | while read -r deployment; do
+        name=$(echo "$deployment" | jq -r '.metadata.name')
+        desired_replicas=$(echo "$deployment" | jq -r '.spec.replicas')
+
+        # Get the pods associated with the deployment
+        pods=$(kubectl get pods -n "$NAMESPACE" -l app="$name" -o json)
+
+        # Check if all pods are ready
+        all_pods_ready=true
+        echo "$pods" | jq -c '.items[]' | while read -r pod; do
+            pod_name=$(echo "$pod" | jq -r '.metadata.name')
+            pod_status=$(echo "$pod" | jq -r '.status.conditions[] | select(.type=="Ready") | .status')
+
+            if [[ "$pod_status" != "True" ]]; then
+                echo "Pod $pod_name in deployment $name is not ready."
+                all_pods_ready=false
+            fi
+        done
+
+        if [[ "$all_pods_ready" == true ]]; then
+            echo "All pods in deployment $name are ready."
+        else
+            all_deployments_ok=false
+        fi
+    done
+
+    # Exit with a status based on the deployments' health
+    if [ "$all_deployments_ok" = true ]; then
+        good "All deployments in namespace $NAMESPACE are fully functional."
+    else
+        warn "Some deployments in namespace $NAMESPACE are not fully functional."
+    fi
 
 }
 
@@ -1044,6 +1097,20 @@ check_kubernetes_processes
 
 echo "Process check completed."
 
+# tests for systemd-resolved
+if ! systemctl is-active systemd-resolved &>/dev/null ; then
+    warn 'systemd-resolved is not running!'
+else
+    good systemd-resolved is active
+fi
+
+# tests for firewalld
+if systemctl is-active firewalld &>/dev/null ; then
+    warn 'firewalld is running, please turn it off using `sudo systemctl disable --now firewalld`'
+else
+    good firewalld is inactive
+fi
+
 #Customers often have time issues, which can cause cert issues.  Ex:cert is in future.
 if type chronyc &>/dev/null;then
   if chronyc activity |grep -q "^0 sources online";then
@@ -1095,6 +1162,83 @@ else
         warn '/opt/cni/bin/bridge not found is your CNI installed?'
     fi
 fi
+
+
+    # Test if containerd directory exists
+    if [ -d '/etc/containerd' ]; then
+        good "/etc/containerd is correctly mounted."
+    else
+        warn "/etc/containerd is not mounted."
+    fi
+
+    # Test if docker directory exists
+    if [ -d '/var/lib/docker' ]; then
+        good "/var/lib/docker exists."
+    else
+        warn "/var/lib/docker does not exist."
+    fi
+
+    # Test for zk, kafka and mongodb directory exists
+    dirs=("/app/nirmata/zk" "/app/nirmata/kafka" "/app/nirmata/mongodb")
+
+    for dir in "${dirs[@]}"; do
+        if [ -d $dir ]; then
+            good "$dir is correctly mounted."
+        else
+            warn "$dir is not mounted."
+        fi
+    done
+
+    # Test for Node Space Allocation (To be modified)
+    paths="/var/nirmata"
+
+    for path in $paths; do
+        if df -h | grep -q "$path"; then
+            space_used=$(df -h | grep "$path" | awk '{print $5}' | sed 's/%//')
+            if [ "$space_used" -gt 50 ]; then
+                warn "Space usage for $path is at $space_used%, which exceeds the 50% threshold."
+            else
+                good "Space allocation for $path is at $space_used%, which is within the 50% threshold."
+            fi
+        else
+            warn "$path is not found."
+        fi
+    done
+
+    # Test for repository access
+    repository_url=$repository
+    pullRes=$(docker pull "$repository_url" 2>&1)
+
+    if [ $? -eq 0 ]; then
+        good "Access to the repository is available and Docker can pull the image."
+    else
+        warn "Cannot access the repository or pull the image."
+    fi
+
+    # Test for docker proxy
+    proxy_conf="/etc/systemd/system/docker.service.d/http-proxy.conf"
+
+    if [ -f "$proxy_conf" ]; then
+        good "Docker is configured to use the proxy."
+    else
+        warn "Docker proxy configuration not found."
+    fi
+
+    echo "Checking for swap"
+    if [[ $(swapon -s | wc -l) -gt 1 ]] ;  then
+        if [[ $fix_issues -eq 0 ]];then
+            warn "Found swap enabled"
+            echo "Applying the following fixes"
+            ech 'swapoff -a'
+            swapoff -a
+            echo "sed -i '/[[:space:]]*swap[[:space:]]*swap/d' /etc/fstab"
+            sed -i '/[[:space:]]*swap[[:space:]]*swap/d' /etc/fstab
+        else
+            error "Found swap enabled!"
+        fi
+    fi
+
+
 }
 
 # Test nirmata agent for nirmata built clusters
